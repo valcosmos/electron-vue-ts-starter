dc6226c06e8db5c7609f27ca4fcea0aa6bb619f8
Cupid Valentine <westerngod@126.com>
2024-11-06 22:27:27 +0800

    feat: add naive and tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,56 @@
 <template>
-  <h1>💖 Hello World!</h1>
-  <p>Welcome to your Electron application.</p>
+  <n-layout class="h-screen w-screen">
+      <n-layout-header class="p-4">
+        Header 
+      </n-layout-header>
+      <n-layout has-sider class="h-[calc(100vh-108px)]">
+        <n-layout-sider
+          bordered
+          show-trigger
+          collapse-mode="width"
+          :collapsed-width="64"
+          :width="240"
+          :native-scrollbar="false"
+          :inverted="inverted"
+        >
+          <n-menu
+            :inverted="inverted"
+            :collapsed-width="64"
+            :collapsed-icon-size="22"
+            :options="menuOptions"
+          />
+        </n-layout-sider>
+        <n-layout>
+          hello world
+
+        </n-layout>
+      </n-layout>
+      <n-layout-footer class="p-4">
+        Footer
+      </n-layout-footer>
+  </n-layout>
 </template>
 
-<script setup>
-console.log('👋 This message is being logged by "App.vue", included via Vite');
+<script lang="ts" setup>
+import type { Component } from 'vue'
+import { defineComponent, h, ref } from 'vue'
+import { NIcon } from 'naive-ui'
+import {
+  BookOutline as BookIcon,
+  PersonOutline as PersonIcon,
+  WineOutline as WineIcon
+} from '@vicons/ionicons5'
+
+function renderIcon(icon: Component) {
+  return () => h(NIcon, null, { default: () => h(icon) })
+}
+
+const menuOptions = [
+  {
+    label: 'Menu 1',
+    key: 'hear-the-wind-sing',
+    icon: renderIcon(BookIcon)
+  },
+
+]
 </script>
