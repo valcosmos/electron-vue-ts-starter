d6b1b29b61fa70003a1622df0c8eaf270cd9c30a
Cupid Valentine <westerngod@126.com>
2024-11-07 23:39:51 +0800

    ci: add github actions

--- forge.config.ts
+++ forge.config.ts
@@ -13,6 +13,18 @@ const config: ForgeConfig = {
   },
   rebuildConfig: {},
   makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'valcosmos',
+          name: 'electron-vue-ts-starter',
+        },
+        prerelease: true,
+      },
+    },
+  ],
   plugins: [
     new VitePlugin({
       // `build` can specify multiple entry builds, which can be Main process, Preload scripts, Worker process, etc.
